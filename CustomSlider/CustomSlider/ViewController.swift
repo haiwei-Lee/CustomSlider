b3ae8d5f7b1b521d81b5611031f6093fc0c5976a
Lee <Lee@MacBook-Air.local>
2015-06-17 15:05:25 +0800

    add project

---
+++ CustomSlider/CustomSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSlider
+//
+//  Created by Lee on 15/6/17.
+//  Copyright (c) 2015年 Havi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
